bcf7ef9f9f8c79a34a1c928b5317d74b5b01c684
David Pleva <dav.plev@seznam.cz>
2020-12-01 23:11:12 +0100

    Test integrated

---
+++ tests/blogs.test.js
@@ -0,0 +1,85 @@
+Number.prototype._called = {};
+
+const Page = require('./helpers/page');
+let page;
+
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+  });
+
+  test('When logged in, can se blog create form', async () => {
+    const label = await page.getContentsOf('.title label');
+    expect(label).toEqual('Blog Title');
+  });
+
+  describe('And using valid inputs', async () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'My title');
+      await page.type('.content input', 'My content');
+      await page.click('form button');
+    });
+
+    test('Submitting takes user to review screen', async () => {
+      //Mohl bych ještě testovat obsah TITLE a CONTENT :) 
+      const submitBtnLabel = await page.getContentsOf('button.green');
+      expect(submitBtnLabel).toMatch('Save Blog');
+    });
+
+    test('Submitting than saving adds blog to index page', async () => {
+      await page.click('button.green');
+      const url = await page.url();
+
+      expect(url).toMatch('http://localhost:3000/blogs');
+    });
+  });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+
+    test('the form shows an error message', async () =>{
+      const titleErr = await page.getContentsOf('.title .red-text');
+      const contentErr = await page.getContentsOf('.content .red-text');
+
+      expect(titleErr).toEqual('You must provide a value');
+      expect(contentErr).toEqual('You must provide a value');
+    });
+  });
+});
+
+describe('User is not logged in', async () => {
+  const actions = [
+    {
+    method: 'get',
+    path: '/api/blogs/'
+  },
+  {
+    method: 'post',
+    path: '/api/blogs/',
+    data: { 
+      title: 'T',
+      content: 'C'
+    }
+  }
+];
+
+  test('Blog related actions are prohibited', async () => {
+    const results = await page.execRequests(actions);
+
+    for (let result of results) {
+      expect(result).toEqual({ error: 'You must log in!'});
+    }
+  });
+});
---
+++ tests/helpers/page.js
@@ -0,0 +1,71 @@
+const puppeteer = require('puppeteer');
+const sessionFactory = require('../factories/sessionFactory');
+const userFactory = require('../factories/userFactory');
+
+class CustomPage {
+  static async build() {
+    const browser = await puppeteer.launch({ 
+      headless: false
+    });
+
+    const page = await browser.newPage();
+    const customPage = new CustomPage(page);
+
+    return new Proxy(customPage, {
+      get: function(target, property) {
+        return customPage[property] || browser[property] || page[property];
+      }
+    });
+  };
+
+  constructor(page) {
+    this.page = page;
+  };
+
+  async login() {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+
+    await this.page.setCookie({ name: 'session', value: session });
+    await this.page.setCookie({ name: 'session.sig', value: sig });
+    await this.page.goto('localhost:3000/blogs');
+    await this.page.waitFor('a[href="/auth/logout"]');
+  };
+
+  async getContentsOf(selector) {
+    return this.page.$eval(selector, el => el.innerHTML);
+  }
+
+  get(route) {
+    return this.page.evaluate(
+      (_route) => {
+        return fetch(_route, {
+          method: 'GET',
+          credentials: 'same-origin',
+          headers: { 'Content-Type': 'application/json'}
+        }).then(res => res.json());
+      }, route);
+  }
+
+  post(route, data) {
+      return this.page.evaluate(
+        (_route, _data) => {
+          return fetch(_route, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(_data)
+          }).then(res => res.json());
+        }, route, data);
+  }
+
+  execRequests(actions) {
+    return Promise.all(
+      actions.map(({method, path, data}) => {
+        return this[method](path, data);
+      })
+    );
+  }
+}
+
+module.exports = CustomPage;
